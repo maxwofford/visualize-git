ff665208bdfc973aab2afe6caa16ebc6d5fe77f3
Max Wofford <max@maxwofford.com>
2025-03-22 15:40:23 -0400

    Log requests

--- main.go
+++ main.go
@@ -118,6 +118,14 @@ func main() {
 	pm := NewProcessManager()
 	r := mux.NewRouter()
 
+	// Add logging middleware
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			log.Printf("Request: %s %s", r.Method, r.URL.Path)
+			next.ServeHTTP(w, r)
+		})
+	})
+
 	// Parse templates
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
